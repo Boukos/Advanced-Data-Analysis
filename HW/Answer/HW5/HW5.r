bea3b44860a882ee4f52a018b43f608b244c8457
lleiou <leoliu0723@gmail.com>
2017-03-10 00:37:54 -0500

    roughly finished...proof read tmr morning.

--- HW/Answer/HW5/HW5.r
+++ HW/Answer/HW5/HW5.r
@@ -6,3 +6,12 @@ glm(ThermalDistress~Temperature, data = data, family = binomial("logit"))
 
 #(c)
 confint(glm(ThermalDistress~Temperature, data = data, family = binomial("logit")))
+
+exp(15.0429-0.2322*31)/(1+exp(15.0429-0.2322*31))
+15.0429/0.2322
+
+#Q2
+#(a)
+data = read.csv("/Users/lleiou/Google Drive/😳 Courses/4th term/Advanced-Data-Analysis/HW/Question/HW5/adolescent.csv")
+
+glm(ThermalDistress~Temperature, data = data, family = binomial("logit"))
