4d1017f0df7e3de9ab02e6849057649a05599016
lleiou <leoliu0723@gmail.com>
2017-04-24 15:19:14 -0400

    save before restart

--- Final Project/final.r
+++ Final Project/final.r
@@ -1,2 +1,24 @@
+library(dplyr)
+data = read.table("/Users/lleiou/Google Drive/😳 Courses/4th term/Advanced-Data-Analysis/Final Project/APPENC02.txt")
+is.na(data[,2])
+sample_n(data,220)
 
-data = read.csv("/Users/lleiou/Google Drive/😳 Courses/4th term/Advanced-Data-Analysis/Final Project/APPENC02.txt")
+
+dim(data)
+dim(data)
+head(data)
+filter(data,data$V2=="Kings", data$V3=="NY")
+slice(data,1:10)
+arrange(data, V2, V1)mutate(data, V18=V4/V5)
+
+filter(summarise(group_by(data, V2), number=n()), number==1)
+
+sapply(data, function(x) sum(is.na(x)))
+
+
+
+# 计算cook‘s distance, 寻找training data 中的outlier并去掉
+# correlation matrix
+# logistic regression plot
+# QQ plot
+# 
